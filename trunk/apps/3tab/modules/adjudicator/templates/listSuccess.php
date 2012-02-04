3013a9e060cd1663b1eab440a0c6f84ef90eae1a
elhh82@gmail.com <elhh82@gmail.com@5aaa9c1c-edc6-8dd8-b063-787d7ad0b76d>
2012-02-04 01:11:33 +0000

    reverted to v69 for adjudicator listSuccess.php

--- trunk/apps/3tab/modules/adjudicator/templates/listSuccess.php
+++ trunk/apps/3tab/modules/adjudicator/templates/listSuccess.php
@@ -30,7 +30,7 @@
 	
 ?>
 <?php foreach ($adjScores as $number => $sScore): ?>
-<?php $adjudicator = $adjudicators[$number];?zzz>
+<?php $adjudicator = $adjudicators[$number];?>
 <tr>
     <td><?php echo link_to($adjudicator->getId(), 'adjudicator/show?id='.$adjudicator->getId()) ?></td>
       <td><?php echo $adjudicator->getName() ?></td>
